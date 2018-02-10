ecfe087ea0ea53a5d6061bac1d6edb53426739e9
Aura Velarde <aura@autoreg-143443.dyn.wpi.edu>
2018-02-10 16:22:10 -0500

    Created the server

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.get('/networkdata', (request, response) => {
     response.sendFile(path.join(__dirname + '/data_shapes/network_links.json'));
   });
 
+  app.get('/networksize', (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.sendFile(path.join(__dirname + '/data_shapes/network_sizes.json'));
+  });
+
+  app.get('/networksummary', (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.sendFile(path.join(__dirname + '/data_shapes/network_summary.json'));
+  });
+
 app.listen(port, (err) => {
     if(err) {
         return console,log('something bad has happened', err)
