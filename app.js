1e29eece5add5d79cb70ec0ddb07332a6ef14aad
Aura Velarde <aura@autoreg-143443.dyn.wpi.edu>
2018-02-10 16:16:28 -0500

    Git ignore, server, index

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+var path = require('path')
+
+app.get('/networkdata', (request, response) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.sendFile(path.join(__dirname + '/data_shapes/network_links.json'));
+  });
+
+app.listen(port, (err) => {
+    if(err) {
+        return console,log('something bad has happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
