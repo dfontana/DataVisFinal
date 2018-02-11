e65b04780af97a6de051d0ff27521442aa3be793
dfontana <dyfontana@gmail.com>
2018-02-10 19:31:16 -0500

    Metadata is ingested.

--- data/build.js
+++ data/build.js
@@ -1,16 +1,28 @@
 const d3 = require('d3')
 const fs = require('fs')
+const JSON5 = require('json5');
 
 
-const metadata = d3.csvParse(fs.readFileSync(__dirname+'/movies_metadata.csv', 'utf8'), function(d) {
-  console.log(d.genres)
-  console.log(JSON.stringify(d.genres))
+const metadata = d3.csvParse(fs.readFileSync(__dirname+'/movie_data/movies_metadata_cleaned.csv', 'utf8'), function(d) {
+  if(d.genres){
+    d.genres = JSON5.parse(d.genres)
+  }
 
-  d.genres = JSON.parse(d.genres),
-  d.production_companies = JSON.parse(d.production_companies),
-  d.production_countries = JSON.parse(d.production_countries),
-  d.belongs_to_collection = JSON.parse(d.belongs_to_collection), 
-  d.spoken_languages = JSON.parse(d.spoken_languages)
+  if(d.production_companies){
+    d.production_companies = JSON5.parse(d.production_companies.replace(/\\xa0/g, ''))
+  }
+
+  if(d.production_countries){
+    d.production_countries = JSON5.parse(d.production_countries)
+  }
+
+  if(d.belongs_to_collection) {
+    d.belongs_to_collection = JSON5.parse(d.belongs_to_collection.replace(/None/g, '""')) 
+  }
+
+  if(d.spoken_languages) {
+    d.spoken_languages = JSON5.parse(d.spoken_languages.replace("\\x9a", 's'))
+  }
   return d;
 });
 
