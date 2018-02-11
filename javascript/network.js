78595456b266bdf7ff1e52c79c93c83dc57f136c
Aura Velarde <aura@Auras-MacBook-Pro.local>
2018-02-11 13:03:20 -0500

    Tool tips over

--- javascript/network.js
+++ javascript/network.js
@@ -31,20 +31,17 @@ var nodes = getNetworkSize().then((data) => {
 })
 
 Promise.all([nodes, links]).then((values) => {
+  var div = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
+  
   var nodes = values[0];
   var links = values[1];
   var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
   var color = d3.scaleOrdinal(d3.schemeCategory20);
-  
   var decade = 2020;
-  console.log(nodes[decade]);
-  console.log(links[decade]);
-
-  var testLink = [{source: 1, target: 9},{source: 1, target: 10}, {source: 10, target: 11},{source: 11, target: 8} ]
- 
-
   
   var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
@@ -61,6 +58,25 @@ Promise.all([nodes, links]).then((values) => {
     .attr("stroke-width", function(d) {
       return d.keywords.length || 0; 
     })
+    .on('mouseover', function(d){
+      div.transition()		
+      .duration(200)		
+      .style("opacity", .9);
+
+      var text = ''
+       d.keywords.forEach((k) => {
+        text += k + '<br>';
+      });
+
+      div.html(text)	
+        .style("left", (d3.event.pageX) + "px")		
+        .style("top", (d3.event.pageY - 28) + "px");	
+    })
+    .on('mouseout', function() {
+      div.transition()		
+      .duration(500)		
+      .style("opacity", 0);	
+    })
 
     var node = svg.append("g")
     .attr("class", "nodes")
@@ -69,10 +85,26 @@ Promise.all([nodes, links]).then((values) => {
     .enter().append("circle")
       .attr("r", function(d) { return d.value; })
       .attr("fill", function(d) { return color(d.id); })
-      // .call(d3.drag()
-      //     .on("start", dragstarted)
-      //     .on("drag", dragged)
-      //     .on("end", dragended));
+      .on('mouseover', function(d){
+        div.transition()		
+        .duration(200)		
+        .style("opacity", .9);	
+        var html = '';
+        Object.keys(d.toolTip).forEach( (obj) => {
+          var toShow = d.toolTip[obj];
+          Object.keys(toShow).forEach( (tool) => {
+            html += capitalize(tool) + ": " + toShow[tool] + "</br>";
+          });
+        });
+        div.html(html)	
+          .style("left", (d3.event.pageX) + "px")		
+          .style("top", (d3.event.pageY - 28) + "px");	
+      })
+      .on('mouseout', function() {
+        div.transition()		
+        .duration(500)		
+        .style("opacity", 0);	
+      })
 
     node.append("title")
     .text(function(d) { return d.id; });
@@ -98,21 +130,6 @@ Promise.all([nodes, links]).then((values) => {
     }
 })
 
-
-
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-}
-
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
