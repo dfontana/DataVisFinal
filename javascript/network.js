d035ad7ebdeedf4ef74667bf6862f2418a6b18a7
Aura Velarde <aura@autoreg-143443.dyn.wpi.edu>
2018-02-15 21:07:10 -0500

    Updated with back end data

--- javascript/network.js
+++ javascript/network.js
@@ -1,38 +1,90 @@
-function getNetworkData() {
+function getNetworkLinks(interest, decade) {
   return new Promise( (resolve, reject) => {
-    d3.json('http://localhost:8000/networkdata', function(data) {
-      resolve(data);
+    d3.json(`http://localhost:8000/${decade}/${interest}/link`, function(data) {
+      resolve(reformLinks(data));
     });
   });
 }
 
-function getNetworkSize() {
+function findIndex(array, item){
+  if(array === undefined) return -1
+  return array.indexOf(e => e.target === item)
+}
+
+/**
+ * Reconstructs the links backed by the compact link
+ * storage algorithm.
+ */
+function reformLinks(keywordLinks){
+  links = {}
+  Object.keys(keywordLinks).map(kID => {
+    let nodes = keywordLinks[kID]
+    for(let i = 0; i < nodes.length; i++){
+      let movieID = nodes[i]
+      for(let j = i+1; j < nodes.length; j++){
+        let otherID = nodes[j]
+
+        let oIDidx = findIndex(links[movieID], otherID)
+        if(oIDidx !== -1){
+          links[movieID][oIDidx].keywords.push(kID)
+          continue;
+        }
+
+        let mIDidx = findIndex(links[otherID], movieID)
+        if(mIDidx !== -1){
+          links[otherID][mIDidx].keywords.push(kID)
+          continue;
+        }
+
+        // Link doesn't exist yet, add it.
+        links[movieID] = (links[movieID] || [])
+        links[movieID].push({
+          source: movieID,
+          target: otherID,
+          keywords: [kID] 
+        })
+      }
+    }
+  })
+  return Object.keys(links).reduce((acc, mID) => [...acc, ...links[mID]], [])
+}
+
+function getNetworkNodes(interest, decade) {
   return new Promise( (resolve, reject) => {
-    d3.json('http://localhost:8000/networksize', function(data) {
+    d3.json(`http://localhost:8000/${decade}/${interest}/size`, function(data) {
+
+      let reshape = Object.keys(data.nodemap).reduce((acc, mID) => {
+        acc.push({
+          id: mID,
+          value: data.nodemap[mID]
+        })
+        return acc;
+      }, [])
+
+      data.nodemap = reshape;
+      resolve(data.nodemap);
+    });
+  })
+}
+
+function getKeywordMap() {
+  return new Promise( (resolve, reject) => {
+    d3.json('http://localhost:8000/wordmap', function(data) {
       resolve(data);
     });
   })
 }
 
-function getNetworkSummary(){
-  return new Promise( (resolve, reject) => {
-    d3.json('http://localhost:8000/networksummary', function(data) {
-      resolve(data);
-    });
-  })
-}
-
-function generateGraph(type, decade) {
-  console.log(type);
-  var links = getNetworkData().then((data => {
-    return data;
-  }))
+function generateGraph(interest, decade) {
+  var links = getNetworkLinks(interest, decade);
   
-  var nodes = getNetworkSize().then((data) => {
-    return data;
-  })
+  var nodes = getNetworkNodes(interest, decade);
+
+   //  nodes.nodemap;
+  //  nodes.namemap;
 
   Promise.all([nodes, links]).then((values) => {
+    console.log(values);
     d3.select('#network').selectAll('*').remove();
     var dropdown = document.getElementById("networkDropdown");
     var section = dropdown.options[dropdown.selectedIndex].value;
@@ -50,21 +102,32 @@ function generateGraph(type, decade) {
     // var decade = decade;
     
     
-    var simulation = d3.forceSimulation()
-      .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    var simulation = d3.forceSimulation(nodes)
+      .force("link", d3.forceLink(links).id(function(d) { return d.id; }))
       .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2));
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .stop();
   
+    for (var i = 0, n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())); i < n; ++i) {
+      simulation.tick();
+    }
+
+    let thickScale = d3.scaleLinear()
+      .domain(d3.extent(links, n => n.keywords.length))
+      .range([1, 5])
+
       var link = 
       svg.append("g")
       .attr("class", "links")
       .selectAll("line")
-      .data(links[decade])
+      .data(links)
       .enter()
       .append("line")
-      .attr("stroke-width", function(d) {
-        return d.keywords.length || 0; 
-      })
+      .attr("stroke-width", d => thickScale(d.keywords.length))
+                  .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; })
       .on('mouseover', function(d){
         div.transition()		
         .duration(200)		
@@ -84,14 +147,20 @@ function generateGraph(type, decade) {
         .duration(500)		
         .style("opacity", 0);	
       })
+
+      let rScale = d3.scaleLinear()
+        .domain(d3.extent(nodes, n => n.value))
+        .range([2, 10])
   
       var node = svg.append("g")
       .attr("class", "nodes")
       .selectAll("circle")
-      .data(nodes[decade])
+      .data(nodes)
       .enter().append("circle")
-        .attr("r", function(d) { return d.value; })
+        .attr("r", function(d) { return rScale(d.value); })
         .attr("fill", function(d) { return color(d.id); })
+                    .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; })
         .on('mouseover', function(d){
           div.transition()		
           .duration(200)		
@@ -116,25 +185,26 @@ function generateGraph(type, decade) {
       node.append("title")
       .text(function(d) { return d.id; });
       
-      simulation
-      .nodes(nodes[decade])
-      .on("tick", ticked);
+      // simulation
+      // .nodes(nodes)
+      // .on("tick", ticked);
   
-      simulation
-      .force("link")
-      .links(links[decade]);
+      // simulation
+      // .force("link")
+      // .links(links);
+
         
-      function ticked() {
-        link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+      // function ticked() {
+      //   link
+      //       .attr("x1", function(d) { return d.source.x; })
+      //       .attr("y1", function(d) { return d.source.y; })
+      //       .attr("x2", function(d) { return d.target.x; })
+      //       .attr("y2", function(d) { return d.target.y; });
       
-        node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-      }
+      //   node
+      //       .attr("cx", function(d) { return d.x; })
+      //       .attr("cy", function(d) { return d.y; });
+      // }
   })
 }
 
