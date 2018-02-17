281108183ccde6d19af05c03f49162ff8f8e972f
Dylan Fontana <dyfontana@gmail.com>
2018-02-16 21:06:29 -0500

    Had to check library in locally, no NPM. 1st Bin tSNE works, others are NULL

--- data/tSNE.js
+++ data/tSNE.js
@@ -5,13 +5,15 @@
  */
 const Bin = require('./buildBin')
 const Keywords = require('./makeTopKeywords')
+const tSNE = require('../deps/tsnejs/tsne')
+const d3 = require('d3')
 const fs = require('fs')
 
 /** 
  * Reformats the array of movie JSON into a feature table for running tSNE.
  * Should not have a header column.
  */
-function tSNE(movies, keywords) {
+function prepData(movies, keywords) {
   return movies.map(m => {
     let features = []
     features.push(parseInt(m.runtime))
@@ -46,12 +48,36 @@ function tSNE(movies, keywords) {
   })
 }
 
-// Build the tSNE files for each bin.
-const bins = [[0, 1950], [1951, 1960], [1961,1970], [1971, 1980], [1981, 1990], [1991, 2000], [2001, 2010], [2011, 2020]]
-bins.map(bin =>{ 
-  console.log('Building Bin', bin[1])
-  let binned = Bin(bin[0], bin[1]);
-  let keywords = Keywords(binned, 50)
-  let tSNEBin = tSNE(binned, keywords)
-  fs.writeFileSync(`${__dirname}/final/tSNE/${bin[1]}.json`, JSON.stringify(tSNEBin), 'utf8', ()=>{});
-})
+function run(data, binLabel) {
+  console.log('Starting tSNE Training for', binLabel)
+  console.log('Features: ', data[0].length)
+  console.log('Examples: ', data.length)
+  console.time("tSNE")
+  let model = new tSNE.tSNE() // Default vals are right
+  model.initDataRaw(data)
+
+  for(let i = 0; i < 500; i++){
+    model.step();
+    if(i % 10 === 0){
+      process.stdout.write(".");
+    }
+  }
+  process.stdout.write("\n");
+
+  let out = model.getSolution();
+  console.timeEnd("tSNE")
+  fs.writeFileSync(`${__dirname}/final/tSNE/${binLabel}-out.json`, JSON.stringify(out), 'utf8', ()=>{});
+}
+
+// Build the tSNE files for each bin if we call from commandline, otherwise we want to use this as an import.
+if(require.main === module){
+  const bins = [[0, 1950], [1951, 1960], [1961,1970], [1971, 1980], [1981, 1990], [1991, 2000], [2001, 2010], [2011, 2020]]
+  bins.map(bin =>{ 
+    console.log('Building Bin', bin[1])
+    let binned = Bin(bin[0], bin[1]);
+    let keywords = Keywords(binned, 50)
+    let tSNEBin = prepData(binned, keywords)
+    run(tSNEBin, bin[1])
+    fs.writeFileSync(`${__dirname}/final/tSNE/${bin[1]}.json`, JSON.stringify(tSNEBin), 'utf8', ()=>{});
+  })
+}
