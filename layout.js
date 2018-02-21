bbb824b6eeac94e59df8b1880064346e5219bb70
Aura Velarde <aura@autoreg-143443.dyn.wpi.edu>
2018-02-21 17:15:47 -0500

    Intro

--- layout.js
+++ layout.js
@@ -1,6 +1,7 @@
 function goToConclusion() {
-  console.log("hello");
-  window.location.href="#conclusion_container"
+document.querySelector('#conclusion_container').scrollIntoView({ 
+  behavior: 'smooth' 
+});
 }
 
 function fastforwardDecade() {
