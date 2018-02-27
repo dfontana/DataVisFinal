c8e63f1934d43c0aade2ae78611c020ec0c5197f
Dylan Fontana <dyfontana@gmail.com>
2018-02-27 01:20:23 -0500

    Removed log and shifted helpers into layoutJS for clarity.

--- javascript/layout.js
+++ javascript/layout.js
@@ -76,3 +76,67 @@ function transitionDecade(decade, keywords, cb) {
 
   setTimeout(() => cb(), 500)
 }
+
+function search(ele) {
+  if(event.key === 'Enter') {
+    let term = ele.value.toLowerCase();
+    filterByTerm(term);
+  }
+}
+
+function filterByTerm(term) {
+  if(term === "") {
+    // Clear the filter
+    dispatch.call('filter', null, null)
+  }else{
+    // Set the filter
+    dispatch.call('filter', null, term)
+  }
+}
+
+function searchForUser(term) {
+  document.getElementById('searchbox').value = term;
+  let finalWord = term.toLowerCase();
+  filterByTerm(finalWord);
+}
+
+function findCoordinatesOf(title){
+  let node = d3.selectAll('.node')
+    .filter(m => {
+      return m[2].title.toLowerCase().includes(title.toLowerCase())
+    })
+  return {x: node.attr('cx'), y: node.attr('cy'), r: 20}  
+}
+
+function takeAction(i, callback){
+  // Dispatch pointer and search events (clear old ones)
+  d3.selectAll('.story-pointer').remove()
+  searchForUser("");
+  let points;
+  switch(i){
+    case 2:
+      points = [ findCoordinatesOf("Arrival of a Train") ]
+      dispatch.call('point-to', this, {points: points, classed:"story-pointer"})
+      break;
+    case 9:
+      searchForUser('Spaghetti Western');
+      break;
+    case 19:
+      searchForUser("Harry Potter and the Philosopher's stone");
+      break;
+    case 20:
+      searchForUser('Harry Potter');
+      break;
+    case 21:
+      searchForUser('The fellowship of the ring')
+      break;
+    case 22:
+      searchForUser('Hero');
+      break;
+    case 24:
+      points = [ findCoordinatesOf("Fury Road"), findCoordinatesOf("gravity") ]
+      dispatch.call('point-to', this, {points: points, classed:"story-pointer"})
+      break;
+  }
+  callback(null);
+}
